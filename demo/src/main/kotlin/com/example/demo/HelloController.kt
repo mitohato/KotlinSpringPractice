6f1b8df63f6b88cdcf6fe24367bf3558ab0cf102
mitohato <mitohato14@gmail.com>
2018-01-12 14:23:48 +0900

    Add japanese greeter interface

--- demo/src/main/kotlin/com/example/demo/HelloController.kt
+++ demo/src/main/kotlin/com/example/demo/HelloController.kt
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-class HelloController {
+class HelloController(private val greeter: Greeter) {
+
     @GetMapping("hello")
-    fun hello(@RequestParam("name") name: String): String = "Hello, $name"
+    fun hello(@RequestParam("name") name: String): String = greeter.hello(name)
 }
